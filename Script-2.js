4f0e27a594b13e9839e44c50a42357698b8f1d8e
KaramatiSyntax <167669114+KaramatiSyntax@users.noreply.github.com>
2024-09-28 21:07:34 +0530

    Update Script-2.js

--- Script-2.js
+++ Script-2.js
@@ -1,23 +1,35 @@
 const loader = document.querySelector(".load");
 const main = document.querySelector("main");
 
+const lists = document.querySelectorAll(".list");
+const indicator = document.querySelector("main nav ul span");
+let desktop = window.matchMedia("(min-width: 1020px)")
+
 window.addEventListener('load', ()=>{
   setTimeout(() =>{
     loader.className = "load hidden";
+    if(desktop.matches){
+      indicator.style.left = `none`
+      indicator.style.top = `${lists[0].offsetTop + 18}px`
+    }else{
+      indicator.style.left = `${lists[0].offsetLeft - 5}px`;
+    }
     setTimeout(() =>{
       loader.style.display = "none";
     }, 2500);
   }, 3000);
 });
 
-const lists = document.querySelectorAll(".list");
-
 function activelink(){
   lists.forEach((item) => 
     item.classList.remove("active"));
     this.classList.add("active");
+    if(desktop.matches){
+      indicator.style.left = `none`
+      indicator.style.top = `${this.offsetTop + 18}px`
+    }else{
+      indicator.style.left = `${this.offsetLeft - 5}px`;
+    }
 }
   lists.forEach((item) => 
   item.addEventListener('click', activelink));
-
- 
